42862d74d39cda5359cd84474bdccab917f8ab5e
Sonia364 <c0872364@mylambton.ca>
2022-12-04 17:24:21 +0000

    Added meal price functionality

--- app/src/main/java/com/sonia/mealordering/MainActivity.java
+++ app/src/main/java/com/sonia/mealordering/MainActivity.java
@@ -3,14 +3,51 @@ package com.sonia.mealordering;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Switch;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-
+    Spinner spinner;
+    EditText price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.mealsList);
+        price = findViewById(R.id.mealPrice);
+
+        Spinner spinner = (Spinner) findViewById(R.id.mealsList);
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.meals, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+        String selectedItem = String.valueOf(adapterView.getItemAtPosition(pos));
+        String priceVal = "10";
+        switch (selectedItem){
+            case "Subway":
+                priceVal = "10";
+                break;
+            case "Taco":
+                priceVal = "20";
+                break;
+        }
+        price.setText(priceVal);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        // Another interface callback
     }
 }

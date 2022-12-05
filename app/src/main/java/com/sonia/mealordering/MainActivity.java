8af70301dd4b3125ffd3d4e44509f3dac8634739
Sonia364 <c0872364@mylambton.ca>
2022-12-05 02:59:50 +0000

    Added seekbar functionality

--- app/src/main/java/com/sonia/mealordering/MainActivity.java
+++ app/src/main/java/com/sonia/mealordering/MainActivity.java
@@ -7,13 +7,18 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner spinner;
     EditText price;
+    SeekBar seekbar;
+    int quantity;
+    final int TAX = 13;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +26,31 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
 
         price = findViewById(R.id.mealPrice);
-
         spinner = findViewById(R.id.mealsList);
+        seekbar = findViewById(R.id.quantitySeekbar);
         spinner.setOnItemSelectedListener(this);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.meals, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                quantity = progressChangedValue;
+//                Toast.makeText(MainActivity.this, "Seek bar progress is :" + progressChangedValue,
+//                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
